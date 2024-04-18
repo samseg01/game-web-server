6cbf41e30ae2aae3104e9f4a779b4c65afc96f95
Samuel Seguins <samuelseguins@gmail.com>
2024-04-18 14:02:55 -0300

    atualização design controle

--- public/scripts/controle-config.js
+++ public/scripts/controle-config.js
@@ -1,14 +1,22 @@
-function tamanho(){
-    const controle = document.querySelector('.controle');
+const controle = document.getElementById('controle')
+const buttons = document.querySelectorAll('button')
+
+setInterval(()=>{
+    buttons.forEach((e)=>{
+        let alturaBtn = getComputedStyle(e).height.split('p')[0];
+        let larguraBtn = getComputedStyle(e).width.split('p')[0];
+
+        if(parseInt(larguraBtn) !== parseInt(alturaBtn)){
+            e.style.width = alturaBtn+'px'
+        }
+    })
     let altura = getComputedStyle(controle).height.split('p')[0];
     let largura = getComputedStyle(controle).width.split('p')[0];
     
     if(parseInt(largura) !== parseInt(2*altura)){
         controle.style.height = (parseInt(largura/2))+'px';
     }
-    
-}
-setInterval(tamanho, 100);
+}, 100);
 
 const socket = io();
 
@@ -20,7 +28,6 @@ window.addEventListener('load', function() {
 socket.on('mensagem', (mensagem) => {
 // console.log('Mensagem do servidor:', mensagem);
 });
-
 
 document.getElementById('esquerda').addEventListener('click', () =>{
     socket.emit('mensagem', 'esquerda');
